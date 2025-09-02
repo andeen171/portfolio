6a0a81e8e16d166605496838450c9d45360d778f
Anderson Lopes <zeander21@outlook.com.br>
2025-09-01 23:52:26 -0300

    fix: add pages router `Layout` for sanity studio

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
